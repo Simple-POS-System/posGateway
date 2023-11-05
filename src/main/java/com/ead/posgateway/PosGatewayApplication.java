b5f90a0f1d6730c04caaab135a071f31d4988535
diniranga <dinirangapremanayake@gmail.com>
2023-11-05 13:58:18 +0530

    initial project file(connected all microservices)

---
+++ src/main/java/com/ead/posgateway/PosGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.ead.posgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PosGatewayApplication.class, args);
+    }
+
+}
